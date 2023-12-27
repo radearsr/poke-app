258c6e57ee21f9e7b38813210586c3007b80c0f0
radearsr <radea.surya1@gmail.com>
2023-12-27 17:47:22 +0700

    update navbar #1

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -7,7 +7,7 @@ import { signOutAuth } from "../../utils/firebase/firebase.utils";
 const Navbar = () => {
   const currentUser = useSelector(selectCurrentUser);
   const favoriteLists = useSelector(selectFavorite);
-  
+
   return (
     <>
       <nav className="flex flex-row justify-between py-2 px-3 bg-cs-black sticky top-0 z-[100]">
@@ -18,22 +18,28 @@ const Navbar = () => {
         </div>
         <ul className="flex flex-row items-center">
           <li>
-            <Link to="/" className="text-cs-yellow">HOME</Link>
+            <Link to="/" className="text-cs-yellow">
+              HOME
+            </Link>
           </li>
           <li className="mx-3">
-            <Link to="/favorite" className="text-cs-yellow">FAVORITE</Link>
+            <Link to="/favorite" className="text-cs-yellow">
+              FAVORITE
+            </Link>
             <span className="ms-1 p-1 text-white bg-cs-orange rounded">
               {favoriteLists.length}
             </span>
           </li>
           <li>
-            {
-              currentUser ? (
-                <button onClick={signOutAuth} className="text-cs-yellow">SIGN OUT</button>
-              ) : (
-                <Link to="/sign-in" className="text-cs-yellow">SIGN IN</Link>
-              )
-            }
+            {currentUser ? (
+              <button onClick={signOutAuth} className="text-cs-yellow">
+                SIGN OUT
+              </button>
+            ) : (
+              <Link to="/sign-in" className="text-cs-yellow">
+                LOGIN
+              </Link>
+            )}
           </li>
         </ul>
       </nav>
